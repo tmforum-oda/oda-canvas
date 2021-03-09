cfd61332c7b03107d207fabf490bfe311f5c9892
brian-burton <brian-burton@users.noreply.github.com>
2021-03-09 20:02:52 +0000

    feat(idm): add seccon listener

--- oda-ca/controllers/securityController/securityController-keycloak.py
+++ oda-ca/controllers/securityController/securityController-keycloak.py
@@ -12,6 +12,28 @@ from cloudevents.http import CloudEvent, to_structured
 
 # Helper functions ----------
 
+def processUrl(url: str) -> str:
+    """
+    This is an horrific bodge until we can add the URL scheme to status.securityAPIs.url
+    """
+    if (url.find("http://") == -1) and (url.find("https://") == -1):
+        url = "http://" + url
+    return url
+
+def registerListener(url: str) -> bool:
+    """
+    Register the listener URL with partyRoleManagement for role updates
+    """
+    try:
+        r = requests.post(url, json = {"callback": "http://seccon.canvas:5000/listener"})
+        #r = requests.post(url, json = {"callback": "http://192.168.3.226:5000/listener"})
+        r.raise_for_status()
+        return True
+    except requests.HTTPError as e:
+        logging.warning(formatCloudEvent(str(e), "secCon couldn't register partyRoleManagement listener"))
+        return False
+
+
 def getToken(user: str, pwd: str) -> str:
     """
     Takes the admin username and password and returns a session token for future Bearer authentication
@@ -21,17 +43,24 @@ def getToken(user: str, pwd: str) -> str:
         r.raise_for_status()
         return r.json()["access_token"]
     except requests.HTTPError as e:
-        logging.warning(reportEvent(str(e), "secCon couldn't GET Keycloak token"))
+        logging.warning(formatCloudEvent(str(e), "secCon couldn't GET Keycloak token"))
 
-def createClient(client: str, token: str, realm: str) -> bool:
+def createClient(client: str, url: str, token: str, realm: str) -> bool:
     """
     POSTs a new client named according to the componentName for a new component
     """
     try:
-        r = requests.post(kcBaseURL + '/admin/realms/'+ realm +'/clients', json={"clientId": client}, headers={'Authorization': 'Bearer ' + token})
+        r = requests.post(kcBaseURL + '/admin/realms/'+ realm +'/clients', json={"clientId": client, "rootUrl": url}, headers={'Authorization': 'Bearer ' + token})
         r.raise_for_status()
         return True
     except requests.HTTPError as e:
+        # ! This might hide actual errors
+        # ! The keycloak API isn't idempotent.
+        # ! If a client exists it returns 409 instead of 201
+        # ! But why did we call createClient for a client that exists?
+        if e.response.status_code == 409:
+            return True # because the client (already) exists, which is what we want
+        else:
             logging.warning(formatCloudEvent(str(e), f"secCon couldn't POST new client {client} in realm {realm}"))
             return False
 
@@ -43,16 +72,22 @@ def delClient(client: str, token: str, realm: str) -> bool:
         r_a = requests.get(kcBaseURL + '/admin/realms/'+ realm +'/clients', params={"clientId": client}, headers={'Authorization': 'Bearer ' + token})
         r_a.raise_for_status()
     except requests.HTTPError as e:
-            logging.warning(formatCloudEvent(str(e), f"secCon couldn't GET ID for client {client} in realm {realm}"))
-            return False
-    targetClient = r_a.json()[0]['id']
+        logging.warning(formatCloudEvent(str(e), f"secCon couldn't GET ID for client {client} in realm {realm}"))
+        return False
+    if len(r_a.json()) == 0:
+        # ! This might hide actual errors
+        # ! if the client doesn't exist the API call returns an empty JSON array
+        # ! But why did we call delClient for a client that didn't exist?
+        return True # because the client doesn't exist, which is what we want
+    else:
+        targetClient = r_a.json()[0]['id']
     try:
         r_b = requests.delete(kcBaseURL + '/admin/realms/'+ realm +'/clients/' + targetClient, headers={'Authorization': 'Bearer ' + token})
         r_b.raise_for_status()
         return True
     except requests.HTTPError as e:
-            logging.warning(formatCloudEvent(str(e), f"secCon couldn't DELETE client {client} in realm {realm}"))
-            return False
+        logging.warning(formatCloudEvent(str(e), f"secCon couldn't DELETE client {client} in realm {realm}"))
+        return False
 
 def formatCloudEvent(message: str, subject: str) -> str:
     """
@@ -61,7 +96,7 @@ def formatCloudEvent(message: str, subject: str) -> str:
     attributes = {
         "specversion" : "1.0",
         "type" : "org.tmforum.for.type.event.an.invented.burton.brian",
-        "source" : "https://example.com/security-controller",
+        "source" : "http://seccon.canvas",
         "subject": subject,
         "id" : str(uuid.uuid4()),
         "time" : datetime.datetime.now().isoformat(),
@@ -92,29 +127,49 @@ logger.setLevel(int(logging_level)) #Logging level default = INFO
 
 # @kopf.on.resume('oda.tmforum.org', 'v1alpha2', 'components')
 @kopf.on.update('oda.tmforum.org', 'v1alpha2', 'components', field='status.deployment_status', value='Complete') # called by kopf framework when a component's status is updated
-def securityRoles(meta, spec, status, body, namespace, labels, name, old, new, **kwargs):
+def securityClientAdd(meta, spec, status, body, namespace, labels, name, old, new, **kwargs):
     """
     Handler for component create/update
     """
     logging.info(f'status.deployment_status = {old} -> {new}')
+    logging.info(f'status is type {type(status)}')
+    logging.info(f"status[\'securityAPIs\']: {status['securityAPIs']}")
     token = getToken(username, password)
-    if not createClient(name, token, kcRealm):
+
+    # ! This commented section is for the old version of status.securityAPIs based on lists
+    #if len(status['securityAPIs']) > 0:
+    #    for i in status['securityAPIs']:
+    #        if i['url'].find("partyRoleManagement"):
+    #            rooturl = processUrl(i['url'])
+    #else:
+    #    raise kopf.TemporaryError("status.SecurityAPIs not populated. Will retry.", delay=10)
+
+    # ! This uncommented section is for the new version of status.securityAPIs based on dicts
+    if 'securityAPIs' in status:
+        rooturl = status['securityAPIs']['partyrole']['url']
+    else:
+        raise kopf.TemporaryError("status.SecurityAPIs not populated. Will retry.", delay=10)
+
+    if not createClient(name, rooturl, token, kcRealm):
         logging.info(formatCloudEvent(f"Could not add oda.tmforum.org component {name}. Will rety.", f"secCon: component create/update"))
-        raise kopf.TemporaryError("Could not add component to Keycloak. Will retry.", delay=20)
+        raise kopf.TemporaryError("Could not add component to Keycloak. Will retry.", delay=10)
     else:
         logging.info(formatCloudEvent(f"oda.tmforum.org component {name} created/updated", f"secCon: component create/update"))
-    statusValue = {'identityProvider': 'Keycloak'}
+    if registerListener(rooturl + "/hub"):
+        statusValue = {'identityProvider': 'Keycloak', 'listenerRegistered': True}
+    else:
+        statusValue = {'identityProvider': 'Keycloak', 'listenerRegistered': False}
     return statusValue # the return value is added to the status field of the k8s object under securityRoles parameter (corresponds to function name)
     
 
 @kopf.on.delete('oda.tmforum.org', 'v1alpha2', 'components') # called by kopf framework when a component is deleted
-def securityRolesDelete(meta, spec, status, body, namespace, labels, name, **kwargs):
+def securityClientDelete(meta, spec, status, body, namespace, labels, name, **kwargs):
     """
     Handler to delete component from Keycloak
     """
     token = getToken(username, password)
     if not delClient(name, token, kcRealm):
         logging.info(formatCloudEvent(f"Could not delete oda.tmforum.org component {name}. Will rety.", f"secCon: component delete"))
-        raise kopf.TemporaryError("Could not delete component from Keycloak. Will rety.", delay=20)
+        raise kopf.TemporaryError("Could not delete component from Keycloak. Will rety.", delay=10)
     else:
         logging.info(formatCloudEvent(f"oda.tmforum.org component {name} deleted", f"secCon: component delete"))
--- oda-ca/controllers/securityController/securityControllerAPIserver-keycloak.py
+++ oda-ca/controllers/securityController/securityControllerAPIserver-keycloak.py
@@ -6,8 +6,100 @@ from flask import Flask
 from flask import request
 import logging
 import os
- 
+import requests
+import uuid
+import datetime
+
+from cloudevents.http import CloudEvent, to_structured
+
+# Helper functions -------------------------------------------------------
+
+def getToken(user: str, pwd: str) -> str:
+    """
+    Takes the admin username and password and returns a session token for future Bearer authentication
+    """
+    try:
+        r = requests.post(kcBaseURL + '/realms/master/protocol/openid-connect/token', data = {"username": user, "password": pwd, "grant_type": "password", "client_id": "admin-cli"})
+        r.raise_for_status()
+        return r.json()["access_token"]
+    except requests.HTTPError as e:
+        logging.warning(formatCloudEvent(str(e), "secCon couldn't GET Keycloak token"))
+
+def formatCloudEvent(message: str, subject: str) -> str:
+    """
+    Returns a correctly formatted CloudEvents compliant event
+    """
+    attributes = {
+        "specversion" : "1.0",
+        "type" : "org.tmforum.for.type.event.an.invented.burton.brian",
+        "source" : "https://example.com/security-controller",
+        "subject": subject,
+        "id" : str(uuid.uuid4()),
+        "time" : datetime.datetime.now().isoformat(),
+        "datacontenttype" : "application/json",
+    }
+
+    data = {"message": message}
+
+    event = CloudEvent(attributes, data)
+    headers, body = to_structured(event)
+
+    return body
+
+def getClientList(token: str, realm: str) -> dict:
+    """
+    GETs a list of clients in the realm to ensure there is a client to match the componentName
+    """
+    try:
+        r = requests.get(kcBaseURL + '/admin/realms/'+ realm +'/clients', headers={'Authorization': 'Bearer ' + token})
+        r.raise_for_status()
+        clientList = dict((d['clientId'], d['id']) for d in r.json())
+        logging.debug(clientList)
+        return clientList
+    except requests.HTTPError as e:
+            logging.warning(formatCloudEvent(str(e), f"secCon couldn't GET clients for {realm}"))
+
+def createRole(role: str, client: str, token: str, realm: str) -> bool:
+    """
+    POSTs a new role to a client named according to the componentName for a new component
+    """
+    try:
+        r = requests.post(kcBaseURL + '/admin/realms/'+ realm +'/clients', json={"clientId": client}, headers={'Authorization': 'Bearer ' + token})
+        r.raise_for_status()
+        return True
+    except requests.HTTPError as e:
+            logging.warning(formatCloudEvent(str(e), f"secCon couldn't POST new client {client} in realm {realm}"))
+            return False
+
+def delRole(role: str, client: str, token: str, realm: str) -> bool:
+    """
+    DELETEs a client
+    """
+    try:
+        r_a = requests.get(kcBaseURL + '/admin/realms/'+ realm +'/clients', params={"clientId": client}, headers={'Authorization': 'Bearer ' + token})
+        r_a.raise_for_status()
+    except requests.HTTPError as e:
+            logging.warning(formatCloudEvent(str(e), f"secCon couldn't GET ID for client {client} in realm {realm}"))
+            return False
+    try: # to find the right client
+        targetClient = r_a.json()[0]['id']
+    except:
+        pass
+    try:
+        r_b = requests.delete(kcBaseURL + '/admin/realms/'+ realm +'/clients/' + targetClient, headers={'Authorization': 'Bearer ' + token})
+        r_b.raise_for_status()
+        return True
+    except requests.HTTPError as e:
+            logging.warning(formatCloudEvent(str(e), f"secCon couldn't DELETE client {client} in realm {realm}"))
+            return False
+
+# Initial setup ----------------------------------------------------------
+
 logging_level = os.environ.get('LOGGING',20)
+username = os.environ.get('KEYCLOAK_USER')
+password = os.environ.get('KEYCLOAK_PASSWORD')
+kcBaseURL = os.environ.get('KEYCLOAK_BASE')
+kcRealm = os.environ.get('KEYCLOAK_REALM')
 print('Logging set to ',logging_level)
 logger = logging.getLogger()
 logger.setLevel(int(logging_level)) #Logging level default = INFO
@@ -19,7 +111,7 @@ PARTY_ROLE_DELETION = 'PartyRoleRemoveNotification'
 app = Flask(__name__)
 @app.route('/listener', methods=['POST'])
 def partyRoleListener():
-    try: # to process incomming POSTs
+    try: # to process incoming POSTs
         doc = request.json
         logging.debug(f"security-APIListener received {doc}")
         partyRole = doc['event']['partyRole']
@@ -29,12 +121,22 @@ def partyRoleListener():
         eventType = doc['eventType']
         logging.info(f"security-APIListener called with eventType {eventType}")
 
+        token = getToken(username, password)
+        clientList = getClientList(token, kcRealm)
+        client = clientList[componentName]
         if eventType==PARTY_ROLE_CREATION:
-            logging.debug(f"Update Keycloak for CREATE")
+            if createRole(partyRole, client, token, kcRealm):
+                logging.debug(formatCloudEvent(f"Keycloak role {partyRole} added to {client}", "secCon event listener success"))
+            else:
+                logging.debug(formatCloudEvent(f"Keycloak role update failed for {partyRole} in {client}", "secCon event listener error"))
         elif eventType==PARTY_ROLE_UPDATE:
-            logging.debug(f"Update Keycloak for UPDATE")
+            pass
+            #logging.debug(f"Update Keycloak for UPDATE")
         elif eventType==PARTY_ROLE_DELETION:
-            logging.debug(f"Update Keycloak for DELETE")
+            if delRole(partyRole, client, token, kcRealm):
+                logging.debug(formatCloudEvent(f"Keycloak role {partyRole} deleted from {client}", "secCon event listener success"))
+            else:
+                logging.debug(formatCloudEvent(f"Keycloak role delete failed for {partyRole} in {client}", "secCon event listener error"))
         else:
             raise Exception(f"Unknown event {eventType}")
 
