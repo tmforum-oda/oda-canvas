ffed81df0f7c4476d66638f5166e5f0b73938930
brian-burton <brian-burton@users.noreply.github.com>
2021-03-26 15:02:55 +0000

    refactor(seccon): move common functions to module

--- oda-ca/controllers/securityController/securityControllerAPIserver-keycloak.py
+++ oda-ca/controllers/securityController/securityControllerAPIserver-keycloak.py
@@ -9,24 +9,25 @@ import os
 import requests
 import uuid
 import datetime
+import secconkeycloak
 
 from cloudevents.http import CloudEvent, to_structured
 
 # Helper functions -------------------------------------------------------
 
-def getToken(user: str, pwd: str) -> str:
-    """
-    Takes the admin username and password and returns a session token for future Bearer authentication
-
-    Returns the token, or raises an exception for the caller to catch
-    """
-
-    try: # to get the token from Keycloak
-        r = requests.post(kcBaseURL + '/realms/master/protocol/openid-connect/token', data = {"username": user, "password": pwd, "grant_type": "password", "client_id": "admin-cli"})
-        r.raise_for_status()
-        return r.json()["access_token"]
-    except requests.HTTPError as e:
-        raise
+#def getToken(user: str, pwd: str) -> str:
+#    """
+#    Takes the admin username and password and returns a session token for future Bearer authentication
+#
+#    Returns the token, or raises an exception for the caller to catch
+#    """
+#
+#    try: # to get the token from Keycloak
+#        r = requests.post(kcBaseURL + '/realms/master/protocol/openid-connect/token', data = {"username": user, "password": pwd, "grant_type": "password", "client_id": "admin-cli"})
+#        r.raise_for_status()
+#        return r.json()["access_token"]
+#    except requests.HTTPError as e:
+#        raise
 
 def formatCloudEvent(message: str, subject: str) -> str:
     """
@@ -49,51 +50,51 @@ def formatCloudEvent(message: str, subject: str) -> str:
 
     return body
 
-def getClientList(token: str, realm: str) -> dict:
-    """
-    GETs a list of clients in the realm to ensure there is a client to match the componentName
+#def getClientList(token: str, realm: str) -> dict:
+#    """
+#    GETs a list of clients in the realm to ensure there is a client to match the componentName
+#
+#    Returns a dictonary of clients and ids or raises an exception for the caller to catch
+#    """
+#    try:
+#        r = requests.get(kcBaseURL + '/admin/realms/'+ realm +'/clients', headers={'Authorization': 'Bearer ' + token})
+#        r.raise_for_status()
+#        clientList = dict((d['clientId'], d['id']) for d in r.json())
+#        return clientList
+#    except requests.HTTPError as e:
+#        raise
 
-    Returns a dictonary of clients and ids or raises an exception for the caller to catch
-    """
-    try:
-        r = requests.get(kcBaseURL + '/admin/realms/'+ realm +'/clients', headers={'Authorization': 'Bearer ' + token})
-        r.raise_for_status()
-        clientList = dict((d['clientId'], d['id']) for d in r.json())
-        return clientList
-    except requests.HTTPError as e:
-        raise
+#def addRole(role: str, clientId: str, token: str, realm: str) -> None:
+#    """
+#    POST new roles to the right client in the right realm in Keycloak
+#
+#    Returns nothing or raises an exception for the caller to catch
+#    """
+#
+#    try: # to add new role to Keycloak
+#        r = requests.post(kcBaseURL + '/admin/realms/'+ realm +'/clients/' + clientId + '/roles', json = {"name": role}, headers={'Authorization': 'Bearer ' + token})
+#        r.raise_for_status()
+#    except requests.HTTPError as e:
+#        if r.status_code == 409:
+#            pass # because the role already exists, which is acceptable but suspicious
+#        else:
+#            raise # because we failed to add the role
 
-def addRole(role: str, clientId: str, token: str, realm: str) -> None:
-    """
-    POST new roles to the right client in the right realm in Keycloak
-
-    Returns nothing or raises an exception for the caller to catch
-    """
-
-    try: # to add new role to Keycloak
-        r = requests.post(kcBaseURL + '/admin/realms/'+ realm +'/clients/' + clientId + '/roles', json = {"name": role}, headers={'Authorization': 'Bearer ' + token})
-        r.raise_for_status()
-    except requests.HTTPError as e:
-        if r.status_code == 409:
-            pass # because the role already exists, which is acceptable but suspicious
-        else:
-            raise # because we failed to add the role
-
-def delRole(role: str, client: str, token: str, realm: str) -> bool:
-    """
-    DELETE removed roles from the right client in the right realm in Keycloak
-
-    Returns nothing or raises an exception for the caller to catch
-    """
-
-    try: # to to remove role from Keycloak
-        r = requests.delete(kcBaseURL + '/admin/realms/'+ realm +'/clients/' + client + '/roles/' + role, headers={'Authorization': 'Bearer ' + token})
-        r.raise_for_status()
-    except requests.HTTPError as e:
-        if r.status_code == 404:
-            pass # because the role does not exist which is acceptable but suspicious
-        else:
-            raise # because we failed to delete the role
+#def delRole(role: str, client: str, token: str, realm: str) -> bool:
+#    """
+#    DELETE removed roles from the right client in the right realm in Keycloak
+#
+#    Returns nothing or raises an exception for the caller to catch
+#    """
+#
+#    try: # to to remove role from Keycloak
+#        r = requests.delete(kcBaseURL + '/admin/realms/'+ realm +'/clients/' + client + '/roles/' + role, headers={'Authorization': 'Bearer ' + token})
+#        r.raise_for_status()
+#    except requests.HTTPError as e:
+#        if r.status_code == 404:
+#            pass # because the role does not exist which is acceptable but suspicious
+#        else:
+#            raise # because we failed to delete the role
 
 # Initial setup ----------------------------------------------------------
 
@@ -109,6 +110,8 @@ logger.setLevel(int(logging_level)) #Logging level default = INFO
 PARTY_ROLE_CREATION = 'PartyRoleCreationNotification'
 PARTY_ROLE_UPDATE = 'PartyRoleAttributeValueChangeNotification'
 PARTY_ROLE_DELETION = 'PartyRoleRemoveNotification'
+
+kc = secconkeycloak.Keycloak(kcBaseURL)
 
 # Flask app --------------------------------------------------------------
 
@@ -126,31 +129,43 @@ def partyRoleListener():
         componentName = partyRole['href'].split('/')[3]
         
         try: # to authenticate and get a token
-            token = getToken(username, password)
+            token = kc.getToken(username, password)
         except requests.HTTPError as e:
             logging.error(formatCloudEvent(str(e), "security-APIListener couldn't GET Keycloak token"))
             raise
+        except requests.URLRequired as e:
+            logging.error(formatCloudEvent(str(e), "security-APIListener couldn't determine Keycloak URL"))
+            raise
 
         try: # to get the list of existing clients
-            clientList = getClientList(token, kcRealm)
+            clientList = kc.getClientList(token, kcRealm)
         except requests.HTTPError as e:
             logging.error(formatCloudEvent(str(e), f"security-APIListener couldn't GET clients for {kcRealm}"))
+        except requests.URLRequired as e:
+            logging.error(formatCloudEvent(str(e), "security-APIListener couldn't determine Keycloak URL"))
+            raise
         else:
             client = clientList[componentName]
 
         if client != "":
             if eventType==PARTY_ROLE_CREATION:
                 try: # to add the role to the client in Keycloak
-                    addRole(partyRole["name"], client, token, kcRealm)
+                    kc.addRole(partyRole["name"], client, token, kcRealm)
                 except requests.HTTPError as e:
                     logging.error(formatCloudEvent(f'Keycloak role create failed for {partyRole["name"]} in {componentName}', "security-APIListener event listener error"))
+                except requests.URLRequired as e:
+                    logging.error(formatCloudEvent(str(e), "security-APIListener couldn't determine Keycloak URL"))
+                    raise
                 else:
                     logging.info(formatCloudEvent(f'Keycloak role {partyRole["name"]} added to {componentName}', "security-APIListener event listener success"))
             elif eventType==PARTY_ROLE_DELETION:
                 try: # to add the role to the client in Keycloak
-                    delRole(partyRole["name"], client, token, kcRealm)
+                    kc.delRole(partyRole["name"], client, token, kcRealm)
                 except requests.HTTPError as e:
                     logging.error(formatCloudEvent(f'Keycloak role delete failed for {partyRole["name"]} in {componentName}', "security-APIListener event listener error"))
+                except requests.URLRequired as e:
+                    logging.error(formatCloudEvent(str(e), "security-APIListener couldn't determine Keycloak URL"))
+                    raise
                 else:
                     logging.info(formatCloudEvent(f'Keycloak role {partyRole["name"]} removed from {componentName}', "security-APIListener event listener success"))
             elif eventType==PARTY_ROLE_UPDATE:
