188f5a5c5626e461b97c23d038a119d061ec6e08
Lester Thomas <lester.thomas@vodafone.com>
2021-03-29 19:13:52 +0100

    Updated python client to kubernetes==17.14.0a1

--- oda-ca/controllers/buildControllers.sh
+++ oda-ca/controllers/buildControllers.sh
@@ -1,6 +1,6 @@
-docker build --file component-IngressController-dockerfile -t brianjburton/odacomponentcontroller-ingress:0.11.0 -t brianjburton/odacomponentcontroller-ingress:latest .
-docker push brianjburton/odacomponentcontroller-ingress --all-tags
+docker build --file component-IngressController-dockerfile -t tmforumodacanvas/component-controller:0.11.0 -t tmforumodacanvas/component-controller:master .
+docker push tmforumodacanvas/component-controller --all-tags
 #docker build --file component-wso2Controller-dockerfile -t brianjburton/odacomponentcontroller-wso2:0.9 -t brianjburton/odacomponentcontroller-wso2:latest .
 #docker push brianjburton/odacomponentcontroller-wso2  --all-tags
-docker build --file securityControllerAPIserver-keycloak-dockerfile -t brianjburton/odaseccon-keycloak:0.3.0 -t brianjburton/odaseccon-keycloak:latest .
-docker push brianjburton/odaseccon-keycloak  --all-tags
+docker build --file securityControllerAPIserver-keycloak-dockerfile -t tmforumodacanvas/security-listener:0.3.0 -t tmforumodacanvas/security-listener:master .
+docker push tmforumodacanvas/security-listener  --all-tags
