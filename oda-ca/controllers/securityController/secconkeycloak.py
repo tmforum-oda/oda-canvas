b8ad072d6ffd78d593d774f8374bf2a22e8f1e3e
Brian Burton <brian-burton@users.noreply.github.com>
2021-04-20 22:01:14 +0100

    chore(secCon): align code with Google Style Guide

--- oda-ca/controllers/securityController/secconkeycloak.py
+++ oda-ca/controllers/securityController/secconkeycloak.py
@@ -5,115 +5,153 @@ class Keycloak:
     def __init__(self, url):
         self._url = url
 
-    def getToken(self, user: str, pwd: str) -> str:
+    def get_token(self, user: str, pwd: str) -> str:
         """
-        Takes the admin username and password and returns a session token for future Bearer authentication
+        Takes the admin username and password and returns a session
+        token for future Bearer authentication
 
-        Returns the token, or raises an exception for the caller to catch
+        Returns the token, or raises an exception for the caller to
+        catch
         """
-        try: # to get the token from Keycloak
-            r = requests.post(self._url + '/realms/master/protocol/openid-connect/token', data = {"username": user, "password": pwd, "grant_type": "password", "client_id": "admin-cli"})
+        try:
+            r = requests.post(
+                self._url + '/realms/master/protocol/openid-connect/token',
+                data = {
+                    'username': user,
+                    'password': pwd,
+                    'grant_type': 'password',
+                    'client_id': 'admin-cli'
+                }
+            )
             r.raise_for_status()
-            return r.json()["access_token"]
-        except (requests.HTTPError, requests.URLRequired) as e:
-            raise
+            return r.json()['access_token']
+        except requests.HTTPError as e:
+            raise RuntimeError(f'get_token failed with HTTP status {r.status_code}: {e}') from None
 
-    def createClient(self, client: str, url: str, token: str, realm: str) -> None:
+    def create_client(self, client: str, url: str, token: str, realm: str) -> None:
         """
-        POSTs a new client named according to the componentName for a new component
+        POSTs a new client named according to the componentName for
+        a new component
 
         Returns nothing, or raises an exception for the caller to catch
         """
 
         try: # to create the client in Keycloak
-            r = requests.post(self._url + '/admin/realms/'+ realm +'/clients', json={"clientId": client, "rootUrl": url}, headers={'Authorization': 'Bearer ' + token})
+            r = requests.post(
+                self._url + '/admin/realms/'+ realm +'/clients',
+                json={'clientId': client, 'rootUrl': url},
+                headers={'Authorization': 'Bearer ' + token}
+            )
             r.raise_for_status()
         except requests.HTTPError as e:
             # ! This might hide actual errors
             # ! The keycloak API isn't idempotent.
             # ! If a client exists it returns 409 instead of 201
-            # ! But why did we call createClient for a client that exists?
-            if e.response.status_code == 409:
-                pass # because the client (already) exists, which is what we want
+            # ! But why did we call create_client for a client that
+            # ! exists?
+            if r.status_code == 409:
+                pass # because the client exists, which is what we want
             else:
-                raise
-        except requests.URLRequired as e:
-            raise
+                raise RuntimeError('create_client failed with HTTP status '
+                                    f'{r.status_code}: {e}') from None
 
-    def delClient(self, client: str, token: str, realm: str) -> None:
+    def del_client(self, client: str, token: str, realm: str) -> None:
         """
         DELETEs a client
 
         Returns nothing, or raises an exception for the caller to catch
         """
-        
+
         try: # to GET the id of the existing client that we need to DELETE it
-            r_a = requests.get(self._url + '/admin/realms/'+ realm +'/clients', params={"clientId": client}, headers={'Authorization': 'Bearer ' + token})
+            r_a = requests.get(
+                self._url + '/admin/realms/'+ realm +'/clients',
+                params={'clientId': client},
+                headers={'Authorization': 'Bearer ' + token}
+            )
             r_a.raise_for_status()
-        except (requests.HTTPError, requests.URLRequired) as e:
-            raise
+        except requests.HTTPError as e:
+            raise RuntimeError('del_client failed to get client ID with HTTP status '
+                                f'{r_a.status_code}: {e}') from None
 
         if len(r_a.json()) > 0: # we found a client with a matching name
-            targetClient = r_a.json()[0]['id']
+            target_client = r_a.json()[0]['id']
 
             try: # to delete the client matching the id we found
-                r_b = requests.delete(self._url + '/admin/realms/'+ realm +'/clients/' + targetClient, headers={'Authorization': 'Bearer ' + token})
+                r_b = requests.delete(
+                    self._url + '/admin/realms/'+ realm +'/clients/' + target_client,
+                    headers={'Authorization': 'Bearer ' + token}
+                )
                 r_b.raise_for_status()
-            except (requests.HTTPError, requests.URLRequired) as e:
-                raise
+            except requests.HTTPError as e:
+                raise RuntimeError('del_client failed to delete client with HTTP status '
+                                    f'{r_b.status_code}: {e}') from None
 
         else: # we didn't find a client with a matching name
             # ! This might hide actual errors
-            # ! if the client doesn't exist the API call returns an empty JSON array
-            # ! But why did we call delClient for a client that didn't exist?
-            pass # because the client doesn't exist, which is what we want
+            # ! If the client doesn't exist the API call returns an
+            # ! empty JSON array, but why did we call del_client for a
+            # ! client that didn't exist?
+            pass # because the client doesn't exist, which is OK
 
-    def getClientList(self, token: str, realm: str) -> dict:
+    def get_client_list(self, token: str, realm: str) -> dict:
         """
-        GETs a list of clients in the realm to ensure there is a client to match the componentName
+        GETs a list of clients in the realm to ensure there is a
+        client to match the componentName
 
-        Returns a dictonary of clients and ids or raises an exception for the caller to catch
+        Returns a dictonary of clients and ids or raises
+        an exception for the caller to catch
         """
         try:
-            r = requests.get(self._url + '/admin/realms/'+ realm +'/clients', headers={'Authorization': 'Bearer ' + token})
+            r = requests.get(
+                self._url + '/admin/realms/'+ realm +'/clients',
+                headers={'Authorization': 'Bearer ' + token}
+            )
             r.raise_for_status()
-            clientList = dict((d['clientId'], d['id']) for d in r.json())
-            return clientList
-        except (requests.HTTPError, requests.URLRequired) as e:
-            raise
+            client_list = dict((d['clientId'], d['id']) for d in r.json())
+            return client_list
+        except requests.HTTPError as e:
+            raise RuntimeError('get_client_list failed with HTTP status '
+                                f'{r.status_code}: {e}') from None
 
-    def addRole(self, role: str, clientId: str, token: str, realm: str) -> None:
+    def add_role(self, role: str, client_id: str, token: str, realm: str) -> None:
         """
-        POST new roles to the right client in the right realm in Keycloak
+        POST new roles to the right client in the right realm in
+        Keycloak
 
         Returns nothing or raises an exception for the caller to catch
         """
 
         try: # to add new role to Keycloak
-            r = requests.post(self._url + '/admin/realms/'+ realm +'/clients/' + clientId + '/roles', json = {"name": role}, headers={'Authorization': 'Bearer ' + token})
+            r = requests.post(
+                self._url + '/admin/realms/'+ realm +'/clients/' + client_id + '/roles',
+                json = {'name': role},
+                headers={'Authorization': 'Bearer ' + token}
+            )
             r.raise_for_status()
         except requests.HTTPError as e:
             if r.status_code == 409:
                 pass # because the role already exists, which is acceptable but suspicious
             else:
-                raise # because we failed to add the role
-        except requests.URLRequired as e:
-            raise
+                raise RuntimeError('add_role failed with HTTP status '
+                                    f'{r.status_code}: {e}') from None
 
-    def delRole(self, role: str, client: str, token: str, realm: str) -> None:
+    def del_role(self, role: str, client: str, token: str, realm: str) -> None:
         """
-        DELETE removed roles from the right client in the right realm in Keycloak
+        DELETE removed roles from the right client in the right realm
+        in Keycloak
 
         Returns nothing or raises an exception for the caller to catch
         """
 
         try: # to to remove role from Keycloak
-            r = requests.delete(self._url + '/admin/realms/'+ realm +'/clients/' + client + '/roles/' + role, headers={'Authorization': 'Bearer ' + token})
+            r = requests.delete(
+                self._url + '/admin/realms/'+ realm +'/clients/' + client + '/roles/' + role,
+                headers={'Authorization': 'Bearer ' + token}
+            )
             r.raise_for_status()
         except requests.HTTPError as e:
             if r.status_code == 404:
                 pass # because the role does not exist which is acceptable but suspicious
             else:
-                raise # because we failed to delete the role
-        except requests.URLRequired as e:
-            raise
+                raise RuntimeError('del_role failed with HTTP status '
+                                    f'{r.status_code}: {e}') from None
