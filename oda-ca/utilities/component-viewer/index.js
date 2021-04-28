f9de87f89d1ae6deca3d375bfb44f23770e8daaa
Lester Thomas <lester.thomas@vodafone.com>
2021-04-28 10:47:11 +0100

    Updated cv utility to use v1alpha3 API

--- oda-ca/utilities/component-viewer/index.js
+++ oda-ca/utilities/component-viewer/index.js
@@ -38,7 +38,7 @@ function displayComponent() {
     try {
         const customk8sApi = kc.makeApiClient(k8s.CustomObjectsApi);
 
-        customk8sApi.listNamespacedCustomObject('oda.tmforum.org', 'v1alpha2', namespace, 'components').then((res) => {
+        customk8sApi.listNamespacedCustomObject('oda.tmforum.org', 'v1alpha3', namespace, 'components').then((res) => {
             console.clear()
             for (var key in res.body.items) {
                 var item = res.body.items[key]
