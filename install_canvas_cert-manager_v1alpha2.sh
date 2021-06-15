a877bc2d8b6f74bdc668a3b4ce216b3def39b841
Brian Burton <brian-burton@users.noreply.github.com>
2021-06-15 15:27:50 +0100

    fix(controllers): bugfix for show and tell
    Various bug fixes to facilitate sprint 3 show and tell

---
+++ install_canvas_cert-manager_v1alpha2.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo ""
+echo "*********************************************************************"
+echo "Configuring Helm repositories"
+echo "*********************************************************************"
+echo ""
+
+helm repo add stable https://charts.helm.sh/stable
+helm repo add codecentric https://codecentric.github.io/helm-charts
+helm repo add incubator https://charts.helm.sh/incubator
+helm repo add bitnami https://charts.bitnami.com/bitnami
+
+echo ""
+echo "*********************************************************************"
+echo "Creating tls certificates"
+echo "*********************************************************************"
+echo ""
+
+bash ./create_tls_certificates_cert-manager_v1alpha2.sh
+
+echo ""
+echo "*********************************************************************"
+echo "Preparing helm chart external dependencies"
+echo "*********************************************************************"
+echo ""
+
+# Keycloak relies on the Bitnami PostgreSQL chart
+pushd canvas/charts/keycloak/
+helm dependency update
+popd
+
+echo ""
+echo "*********************************************************************"
+echo "Installing base canvas"
+echo "*********************************************************************"
+echo ""
+
+# Example command line install if you ened to change ingress controller
+#helm install --create-namespace --namespace canvas canvas canvas/ --set controller.deployment.ingressClass.name=traefik,controller.deployment.ingressClass.enabled=true,global.clusterCABundle=`cat cabundle.pem.b64`
+helm install --create-namespace --namespace canvas canvas canvas/ --set global.clusterCABundle=`cat cabundle.pem.b64`
+retVal=$?
+
+if [ $retVal -ne 0 ]; then
+    echo ""
+    echo "Canvas base install failed"
+    exit 1
+fi
+
+echo ""
+echo "*********************************************************************"
+echo "Install script finished"
+echo "*********************************************************************"
+exit 0
+
