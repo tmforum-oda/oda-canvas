c65635b5af06bfc8a599e1abcbcdc79e50706489
Lester Thomas <lester.thomas@vodafone.com>
2022-12-06 11:43:32 +0000

    Refactored to make conversions easier to read

--- oda-ca/webhooks/builddockerfile.sh
+++ oda-ca/webhooks/builddockerfile.sh
@@ -1,3 +1,3 @@
-docker build -t tmforumodacanvas/compcrdwebhook:0.3 -f webhook-dockerfile . 
+docker build -t lesterthomas/compcrdwebhook:0.4 -f webhook-dockerfile . 
 #docker build -t tmforumodacanvas/compcrdwebhook:0.3 -t tmforumodacanvas/compcrdwebhook:latest -f webhook-dockerfile . 
-docker push tmforumodacanvas/compcrdwebhook --all-tags
+docker push lesterthomas/compcrdwebhook --all-tags
--- oda-ca/webhooks/implementation/app.js
+++ oda-ca/webhooks/implementation/app.js
@@ -1,133 +1,10 @@
-var fs = require('fs');
-var https = require('https');
-var http = require('http');
-var express = require('express');
-var app = express();
-app.use(express.json())    // <==== parse request body as JSON
-var httpsServer, httpServer;
-// get command line arguments
-var args = process.argv.slice(2);
-
-// the first argument shows if we are in test mode or not
-var testMode = args[0] === 'test';
-
-if (testMode) {
-    console.log('Running in test mode');
-    var httpServer = http.createServer(app);
-
-} else {
-    console.log('Running in production mode');
-    var privateKey  = fs.readFileSync('/etc/secret-volume/tls.key', 'utf8');
-    var certificate = fs.readFileSync('/etc/secret-volume/tls.crt', 'utf8');
-    var credentials = {key: privateKey, cert: certificate};
-    var httpsServer = https.createServer(credentials, app);
-}
-
-
-
-
-
-
-
-
-app.post("/", (req, res, next) => {
-
-  console.log(JSON.stringify(req.body, null, 2))
-  var uid = req.body.request.uid;
-  var desiredAPIVersion = req.body.request.desiredAPIVersion;
-  var objectsArray = req.body.request.objects
-
-  for (var key in objectsArray) {
-    objectsArray[key].metadata.annotations.webhookconverted = "Webhook converted From " + objectsArray[key].apiVersion + " to " + desiredAPIVersion;
-    console.log('Comparing old version ' + objectsArray[key].apiVersion + ' and desired version ' + desiredAPIVersion);
-
-    // if the oldAPIVersion is v1alpha3 or v1alpha4 and newVersion is v1alpha2 or v1alpha1 then convert dependentAPIs dependantAPIs
-    if ((["oda.tmforum.org/v1alpha3", "oda.tmforum.org/v1alpha4"].includes(objectsArray[key].apiVersion)) && (["oda.tmforum.org/v1alpha2", "oda.tmforum.org/v1alpha1"].includes(desiredAPIVersion))) {
-      console.log("convert dependentAPIs to dependantAPIs")
-      if (objectsArray[key].spec.coreFunction.dependentAPIs) {
-        console.log("converting ")
-        objectsArray[key].spec.coreFunction.dependantAPIs = objectsArray[key].spec.coreFunction.dependentAPIs;
-        delete objectsArray[key].spec.coreFunction.dependentAPIs
-      }
-    }
-
-    // if the oldAPIVersion is v1alpha2 or v1alpha1 and newVersion is v1alpha3 or v1alpha4 then convert dependantAPIs dependentAPIs
-    if ((["oda.tmforum.org/v1alpha2", "oda.tmforum.org/v1alpha1"].includes(objectsArray[key].apiVersion)) && (["oda.tmforum.org/v1alpha3", "oda.tmforum.org/v1alpha4"].includes(desiredAPIVersion))) {
-      console.log("convert depandantAPIs to dependentAPIs")
-      if (objectsArray[key].spec.coreFunction.dependantAPIs) {
-        console.log("converting ")
-        objectsArray[key].spec.coreFunction.dependentAPIs = objectsArray[key].spec.coreFunction.dependantAPIs;
-        delete objectsArray[key].spec.coreFunction.dependantAPIs
-      }
-    }
-
-    // if the oldAPIVersion is v1alpha1, v1alpha2, v1alpha3 or v1alpha4 and newVersion is v1beta1 then remove the componentKinds
-    // and add dependentAPIs to the management and security segments
-    if ((["oda.tmforum.org/v1alpha1", "oda.tmforum.org/v1alpha2", "oda.tmforum.org/v1alpha3", "oda.tmforum.org/v1alpha4"].includes(objectsArray[key].apiVersion)) && (["oda.tmforum.org/v1beta1"].includes(desiredAPIVersion))) {
-      console.log("remove componentKinds")
-      if (objectsArray[key].spec.componentKinds) {
-        delete objectsArray[key].spec.componentKinds
-      }
-
-      console.log("add dependentAPIs to management segment")
-      managementArray = objectsArray[key].spec.management
-      delete objectsArray[key].spec.management
-      objectsArray[key].spec.management = {dependentAPIs: []}
-      objectsArray[key].spec.management.exposedAPIs = managementArray
-
-      console.log("add dependentAPIs to security segment")
-      objectsArray[key].spec.security.dependentAPIs = []
-      objectsArray[key].spec.security.exposedAPIs = []
-      
-      if (objectsArray[key].spec.security.partyrole) {
-        // add the partyrole to the exposedAPIs
-        objectsArray[key].spec.security.partyrole.name = "partyrole"
-        objectsArray[key].spec.security.exposedAPIs.push(objectsArray[key].spec.security.partyrole)
-        delete objectsArray[key].spec.security.partyrole
-      }
-    }
-    // if the oldAPIVersion is v1beta1 and newVersion is v1alpha1, v1alpha2, v1alpha3 or v1alpha4 then add the componentKinds
-    // and remove dependentAPIs from the management and security segments
-    if ((["oda.tmforum.org/v1beta1"].includes(objectsArray[key].apiVersion)) && (["oda.tmforum.org/v1alpha1", "oda.tmforum.org/v1alpha2", "oda.tmforum.org/v1alpha3", "oda.tmforum.org/v1alpha4"].includes(desiredAPIVersion))) {
-      console.log("add componentKinds")
-      objectsArray[key].spec.componentKinds = []
-
-      console.log("remove dependentAPIs from management segment")
-      managementArray = objectsArray[key].spec.management.exposedAPIs
-      delete objectsArray[key].spec.management
-      objectsArray[key].spec.management = managementArray
-
-      console.log("remove dependentAPIs from security segment")
-      // find the partyrole in the exposedAPIs and add it to the security
-      for (var i = 0; i < objectsArray[key].spec.security.exposedAPIs.length; i++) {
-        if (objectsArray[key].spec.security.exposedAPIs[i].name === "partyrole") {
-          objectsArray[key].spec.security.partyrole = objectsArray[key].spec.security.exposedAPIs[i]
-        }
-      }
-      delete objectsArray[key].spec.security.exposedAPIs
-      delete objectsArray[key].spec.security.dependentAPIs
-    }
-
-   
-
-    objectsArray[key].apiVersion = desiredAPIVersion;
-  }
-
-    var response = {
-        "apiVersion": "apiextensions.k8s.io/v1beta1",
-        "kind": "ConversionReview",
-        "response": {
-          "uid": uid, // must match <request.uid>
-          "result": {
-            "status": "Success"
-          },  
-        "convertedObjects": objectsArray 
-        }  
-    }
-    console.log(JSON.stringify(response, null, 2))
-
-
 /* 
+
+Webhook to automatically convert between versions for the Component CRD
+The Webhook receives a POST request containing a ConversionReview resource.
+It returns an updated ConversionReview resource with the converted object
+
+Example response:
     {
         "apiVersion": "apiextensions.k8s.io/v1",
         "kind": "ConversionReview",
@@ -171,7 +48,133 @@ app.post("/", (req, res, next) => {
         }
       }
      */
-    //console.log(JSON.stringify(response, null, 2))
+
+var fs = require('fs');
+var https = require('https');
+var http = require('http');
+var express = require('express');
+var app = express();
+app.use(express.json())    // <==== parse request body as JSON
+var httpsServer, httpServer;
+
+// get command line arguments
+var args = process.argv.slice(2);
+
+// the first argument shows if we are in test mode or not
+var testMode = args[0] === 'test';
+
+if (testMode) {
+    console.log('Running in test mode');
+    var httpServer = http.createServer(app);
+
+} else {
+    console.log('Running in production mode');
+    var privateKey  = fs.readFileSync('/etc/secret-volume/tls.key', 'utf8');
+    var certificate = fs.readFileSync('/etc/secret-volume/tls.crt', 'utf8');
+    var credentials = {key: privateKey, cert: certificate};
+    var httpsServer = https.createServer(credentials, app);
+}
+
+app.post("/", (req, res, next) => {
+
+  console.log(JSON.stringify(req.body, null, 2))
+  var uid = req.body.request.uid;
+  var desiredAPIVersion = req.body.request.desiredAPIVersion;
+  var objectsArray = req.body.request.objects
+
+  for (var key in objectsArray) {
+
+    // create APIVersion object based on objectsArray[key].apiVersion and desiredAPIVersion
+    var apiVersion = new APIVersion(objectsArray[key].apiVersion, desiredAPIVersion);
+
+    objectsArray[key].metadata.annotations.webhookconverted = "Webhook converted From " + objectsArray[key].apiVersion + " to " + desiredAPIVersion;
+    console.log('Comparing old version ' + objectsArray[key].apiVersion + ' and desired version ' + desiredAPIVersion);
+
+    // if the oldAPIVersion is v1alpha3 or v1alpha4 and newVersion is v1alpha2 or v1alpha1 then convert dependentAPIs dependantAPIs
+    if (apiVersion.mapOldToNew(["v1alpha3", "v1alpha4"], ["v1alpha2", "v1alpha1"])) {
+      console.log("convert dependentAPIs to dependantAPIs")
+      if (objectsArray[key].spec.coreFunction.dependentAPIs) {
+        objectsArray[key].spec.coreFunction.dependantAPIs = objectsArray[key].spec.coreFunction.dependentAPIs;
+        delete objectsArray[key].spec.coreFunction.dependentAPIs
+      }
+    }
+
+    // if the oldAPIVersion is v1alpha2 or v1alpha1 and newVersion is v1alpha3 or v1alpha4 then convert dependantAPIs dependentAPIs
+    if (apiVersion.mapOldToNew(["v1alpha2", "v1alpha1"], ["v1alpha3", "v1alpha4"])) {
+      console.log("convert depandantAPIs to dependentAPIs")
+      if (objectsArray[key].spec.coreFunction.dependantAPIs) {
+        objectsArray[key].spec.coreFunction.dependentAPIs = objectsArray[key].spec.coreFunction.dependantAPIs;
+        delete objectsArray[key].spec.coreFunction.dependantAPIs
+      }
+    }
+
+    // if the oldAPIVersion is v1alpha1, v1alpha2, v1alpha3 or v1alpha4 and newVersion is v1beta1 then remove the componentKinds
+    // and add dependentAPIs to the management and security segments
+    if (apiVersion.mapOldToNew(["v1alpha1", "v1alpha2", "v1alpha3", "v1alpha4"], ["v1beta1"])) {
+      console.log("remove componentKinds")
+      if (objectsArray[key].spec.componentKinds) {
+        delete objectsArray[key].spec.componentKinds
+      }
+
+      console.log("add dependentAPIs to management segment")
+      managementArray = objectsArray[key].spec.management
+      delete objectsArray[key].spec.management
+      objectsArray[key].spec.management = {dependentAPIs: []}
+      objectsArray[key].spec.management.exposedAPIs = managementArray
+
+      console.log("add dependentAPIs to security segment")
+      objectsArray[key].spec.security.dependentAPIs = []
+      objectsArray[key].spec.security.exposedAPIs = []
+
+      if (objectsArray[key].spec.security.partyrole) {
+        console.log("move the partyrole to the exposedAPIs")
+        // add the partyrole to the exposedAPIs
+        objectsArray[key].spec.security.partyrole.name = "partyrole"
+        objectsArray[key].spec.security.exposedAPIs.push(objectsArray[key].spec.security.partyrole)
+        delete objectsArray[key].spec.security.partyrole
+      }
+    }
+
+    // if the oldAPIVersion is v1beta1 and newVersion is v1alpha1, v1alpha2, v1alpha3 or v1alpha4 then add the componentKinds
+    // and remove dependentAPIs from the management and security segments
+    if (apiVersion.mapOldToNew(["v1beta1"], ["v1alpha1", "v1alpha2", "v1alpha3", "v1alpha4"])) {
+      console.log("add componentKinds")
+      objectsArray[key].spec.componentKinds = []
+
+      console.log("remove dependentAPIs from management segment")
+      managementArray = objectsArray[key].spec.management.exposedAPIs
+      delete objectsArray[key].spec.management
+      objectsArray[key].spec.management = managementArray
+
+      // find the partyrole in the exposedAPIs and add it to the security
+      for (var i = 0; i < objectsArray[key].spec.security.exposedAPIs.length; i++) {
+        if (objectsArray[key].spec.security.exposedAPIs[i].name === "partyrole") {
+          console.log("move the partyrole from the exposedAPIs")
+          objectsArray[key].spec.security.partyrole = objectsArray[key].spec.security.exposedAPIs[i]
+        }
+      }
+      delete objectsArray[key].spec.security.exposedAPIs
+
+      console.log("remove dependentAPIs from security segment")
+      delete objectsArray[key].spec.security.dependentAPIs
+    }
+
+    objectsArray[key].apiVersion = desiredAPIVersion;
+  }
+
+    var response = {
+        "apiVersion": "apiextensions.k8s.io/v1beta1",
+        "kind": "ConversionReview",
+        "response": {
+          "uid": uid, // must match <request.uid>
+          "result": {
+            "status": "Success"
+          },  
+        "convertedObjects": objectsArray 
+        }  
+    }
+    console.log(JSON.stringify(response, null, 2))
+
     res.json(response);
    });
 
@@ -193,3 +196,20 @@ if (testMode) {
   }
 
      
+// Class to capture the old and new API versions
+class APIVersion {
+  constructor(oldAPIVersion, newAPIVersion) {
+    this.oldAPIVersion = oldAPIVersion.split('/')[1]; // only use the version part of the API version i.e. remove ''
+    this.newAPIVersion = newAPIVersion.split('/')[1];
+  }
+
+  // test if array of oldAPIVersions contains the oldAPIVersion and array of newAPIVersions contains the newAPIVersion
+  mapOldToNew(oldAPIVersionList, newAPIVersionList) {
+    if (oldAPIVersionList.includes(this.oldAPIVersion) && newAPIVersionList.includes(this.newAPIVersion)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
