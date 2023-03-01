9c8e2e906c87ff6da70986a0567293d97a87f411
Christos Sotiriou <christos.sotiriou@vodafone.com>
2023-03-01 13:43:25 +0200

    build the images  for the compwebhook for other architectures as well

--- oda-ca/webhooks/builddockerfile.sh
+++ oda-ca/webhooks/builddockerfile.sh
@@ -1,3 +1,7 @@
-docker build -t tmforumodacanvas/compcrdwebhook:0.5.1 -f webhook-dockerfile . 
-#docker build -t tmforumodacanvas/compcrdwebhook:0.3 -t tmforumodacanvas/compcrdwebhook:latest -f webhook-dockerfile . 
-docker push tmforumodacanvas/compcrdwebhook --all-tags
+#buildx is required for this to work
+#docker build -t tmforumodacanvas/compcrdwebhook.3 -t tmforumodacanvas/compcrdwebhook:latest -f webhook-dockerfile .
+#docker push tmforumodacanvas/compcrdwebhook --all-tags
+
+# The following uses buildx to build for multiple platforms (*nix/amd64 and arm64)
+# please read this as well https://github.com/docker/buildx/issues/59 , doesn't change much but it's good to know
+docker buildx build -t "tmforumodacanvas/compcrdwebhook:0.5.1" --platform "linux/amd64,linux/arm64" -f webhook-dockerfile . --push
