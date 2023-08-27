1cc9a83d0e8bc9d2a6fd760ab3aeb961916c8b91
Victor Mari Rodriguez <vrodriguez@tmforum.org>
2023-08-27 22:02:50 +0200

    Reverting previous changes until commit Merge pull request #102 from tmforum-oda/canvas-v1beta2 d8556e0cb76edb3aaad84d24b7f8e209749faa68

--- oda-ca/controllers/KOPFDevInstall.sh
+++
@@ -1,2 +0,0 @@
-echo 'Installing dependent applications and python libraries (kopf, kubernetes, pykube-ng, pyyaml) to allow manual execution of KOPF python modules.'
-sudo pip3 install -r requirements.txt
