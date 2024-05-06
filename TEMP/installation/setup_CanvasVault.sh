9952db4dc3c997f66aacf95758e81bd12d07a6a0
Ferenc Hechler <ferenc.hechler@telekom.de>
2024-05-06 18:16:29 +0200

    update settings for VPS2.

--- TEMP/installation/setup_CanvasVault.sh
+++ TEMP/installation/setup_CanvasVault.sh
@@ -5,8 +5,8 @@ set -e
 Y='\033[0;33m'
 NC='\033[0m' # No Color
 
-if [ ! $1 ] || ([ "$1" != "AWS" ] && [ "$1" != "GCP" ]) ; then
-	echo "COMMAND [AWS|GCP]"
+if [ ! $1 ] || ([ "$1" != "AWS" ] && [ "$1" != "GCP" ] && [ "$1" != "VPS2" ]) ; then
+	echo "COMMAND [AWS|GCP|VPS2]"
 	echo ""
 	echo "Deploy and configure HashiCorp Vault as canvas vault"
     exit 1
@@ -29,10 +29,15 @@ helm repo update
 helm upgrade --install canvas-vault-hc hashicorp/vault --namespace canvas-vault --create-namespace --values canvas-vault-hc/values.yaml --wait
              # --version 0.24.0
 
-sleep 5
+echo "waiting up to 30 seconds for the vault to be ready"
+kubectl -n canvas-vault wait -l  statefulset.kubernetes.io/pod-name=canvas-vault-hc-0 --for=condition=ready pod --timeout=30s
 
 echo -e "${Y}Creating public route to canvas vault${NC}"
-kubectl apply -f canvas-vault-hc/canvas-vault-hc-vs.yaml
+if [ "$1" == "VPS2" ]; then
+    kubectl apply -f canvas-vault-hc/canvas-vault-hc-vs-VPS2.yaml
+else 
+    kubectl apply -f canvas-vault-hc/canvas-vault-hc-vs.yaml
+fi
 
 
 echo -e "${Y}Configuring HashiCorp Vault to accept K8S Service Account Issuer${NC}"
@@ -47,22 +52,26 @@ fi
 # see also: https://developer.hashicorp.com/vault/docs/auth/jwt/oidc-providers/kubernetes#using-service-account-issuer-discovery
 CRB=`kubectl get clusterrolebinding oidc-reviewer 2>/dev/null | grep "service-account-issuer-discovery" || true`
 if [ "$CRB" == "" ]; then
-    echo -e "\t${Y}create cluster role binding${NC}"
+    echo -e "\t${Y}create cluster role binding oidc-reviewer${NC}"
     kubectl create clusterrolebinding oidc-reviewer  --clusterrole=system:service-account-issuer-discovery --group=system:unauthenticated
 else 
-    echo -e "\t${Y}cluster role binding already exits${NC}"
+    echo -e "\t${Y}cluster role binding oidc-reviewer already exits${NC}"
 fi
 
-echo -e "\t${Y}exec vault write${NC}"
+echo -e "\t${Y}exec vault write oidc_discovery_url${NC}"
 if [ "$1" == "AWS" ]; then
 # setup on AWS
     kubectl exec -n canvas-vault -it canvas-vault-hc-0 -- vault write auth/jwt-k8s-cv/config oidc_discovery_url=https://kubernetes.default.svc.cluster.local oidc_discovery_ca_pem=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
     ##old kubectl exec -n canvas-vault -it canvas-vault-hc-0 -- vault write auth/jwt-k8s-cv/config oidc_discovery_url=https://container.googleapis.com/v1/projects/tmforum-oda-component-cluster/locations/europe-west3/clusters/ihc-dt oidc_discovery_ca_pem=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 fi
-if [ "$1" == "GCP" ]; then
-    # setup on GCP
+if [ "$1" == "GCP" || "$1" == "VPS2" ]; then
+    # setup on GCP / VPS2
     ISSUER="$(kubectl get --raw /.well-known/openid-configuration | jq -r '.issuer')"
-    # kubectl exec -n canvas-vault -it canvas-vault-hc-0 -- vault write auth/jwt-k8s-cv/config oidc_discovery_url=$ISSUER
+    kubectl exec -n canvas-vault -it canvas-vault-hc-0 -- vault write auth/jwt-k8s-cv/config oidc_discovery_url=$ISSUER
+fi
+if [ "$1" == "VPS2" ]; then
+    # setup on VPS2
+    ISSUER="$(kubectl get --raw /.well-known/openid-configuration | jq -r '.issuer')"
     kubectl exec -n canvas-vault -it canvas-vault-hc-0 -- vault write auth/jwt-k8s-cv/config oidc_discovery_url=$ISSUER oidc_discovery_ca_pem=@/var/run/secrets/kubernetes.io/serviceaccount/ca.crt 
 fi
 
