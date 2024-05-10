22ec1cdf9aee241ad884087f57cbc1bcb1875878
Ferenc Hechler <ferenc.hechler@telekom.de>
2024-05-10 17:55:08 +0200

    fix sidecar path.

--- TEMP/build/build-componentvault-sidecar.sh
+++ TEMP/build/build-componentvault-sidecar.sh
@@ -7,7 +7,7 @@ cd $(dirname -- $0)
 GIT_COMMIT_SHA=$(git rev-parse --short HEAD)
 NOW=$(date -Iseconds)
       
-cd ../../source/operators/componentvaultoperator-hc/sidecar
+cd ../../source/operators/componentvaultoperator-hc/sidecar/docker
       
 docker build -t mtr.devops.telekom.de/magenta_canvas/public:componentvault-controller-0.1.0-rc \
 	--build-arg "GIT_COMMIT_SHA=$GIT_COMMIT_SHA" \
