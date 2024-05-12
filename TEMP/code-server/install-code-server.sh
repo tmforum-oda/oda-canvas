e1efd169fbf626e7c4b2f8e32c7f5bb24cfca797
ferenc-hechler <github@fh.anderemails.de>
2024-05-13 00:27:39 +0200

    fix "rm[ ]-rf".

--- TEMP/code-server/install-code-server.sh
+++ TEMP/code-server/install-code-server.sh
@@ -8,7 +8,7 @@ test -n "$CODE_SERVER_PASSWORD"
 kubectl create ns code-server || true
 kubectl create secret generic -n code-server code-server-secret --from-literal=password="$CODE_SERVER_PASSWORD" --dry-run=client -oyaml | kubectl apply -f - 
 
-rm-rf code-server-repo
+rm -rf code-server-repo
 git clone https://github.com/coder/code-server code-server-repo
 cd code-server-repo
 helm upgrade --install -n code-server --create-namespace code-server --set existingSecret=code-server-secret ci/helm-chart \
