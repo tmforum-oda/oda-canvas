c720b94b51325abd836402baa4d76165e347c70a
Ferenc Hechler <github@fh.anderemails.de>
2024-05-16 10:53:09 +0200

    update compop version to 0.4.2-sman.

--- TEMP/build/build-component-operator.sh
+++ TEMP/build/build-component-operator.sh
@@ -8,13 +8,13 @@ GIT_COMMIT_SHA=$(git rev-parse --short HEAD)
 NOW=$(date -Iseconds)
       
 cd ../../source/operators
-docker build -t mtr.devops.telekom.de/magenta_canvas/public:component-istio-controller-0.4.1-sman \
+docker build -t mtr.devops.telekom.de/magenta_canvas/public:component-istio-controller-0.4.2-sman \
 	--build-arg "GIT_COMMIT_SHA=$GIT_COMMIT_SHA" \
     --build-arg "CICD_BUILD_TIME=$NOW" \
     --build-arg http_proxy=$http_proxy \
     --build-arg https_proxy=$https_proxy \
     --build-arg no_proxy=$no_proxy \
 	-f component-IstioController-dockerfile .
-docker push mtr.devops.telekom.de/magenta_canvas/public:component-istio-controller-0.4.1-sman
+docker push mtr.devops.telekom.de/magenta_canvas/public:component-istio-controller-0.4.2-sman
 
 kubectl rollout restart deployment -n canvas oda-controller-ingress || true
