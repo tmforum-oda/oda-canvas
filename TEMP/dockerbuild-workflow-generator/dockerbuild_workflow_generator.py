bd56e499f0f66817e40d8b0bb2effa9a26749903
ferenc-hechler <github@fh.anderemails.de>
2024-07-26 21:03:07 +0200

    use jinja2 templates for generating prerelease workflows.

---
+++ TEMP/dockerbuild-workflow-generator/dockerbuild_workflow_generator.py
@@ -0,0 +1,36 @@
+from jinja2 import Template, Environment, PackageLoader, select_autoescape
+import yaml
+import os
+
+def to_yaml(data):
+    return yaml.dump(data)
+
+def run(config_filename:str):
+    output_folder = "output"
+    os.makedirs(output_folder, exist_ok=True)   
+
+    env = Environment(
+        loader=PackageLoader("dockerbuild_workflow_generator"),
+        autoescape=select_autoescape()
+    )
+    env.filters.update({'to_yaml': to_yaml})
+
+    template_name = "dockerbuild-prerelease"
+    
+    template = env.get_template(f"{template_name}.yml.jinja2")
+    
+    with open(config_filename, 'r') as f:
+        cfgs = yaml.safe_load(f)
+
+    for filename,cfg in cfgs.items():
+        if filename.startswith("_"):
+            continue
+        if "fileName" not in cfg:
+            cfg["filename"] = name
+        output_filename = f"{output_folder}/{template_name}-{filename}.yml"
+        print(f"generating {output_filename}")
+        template.stream(cfg).dump(output_filename)
+
+if __name__ == '__main__':
+    run("dockerbuild-config.yaml")
+
