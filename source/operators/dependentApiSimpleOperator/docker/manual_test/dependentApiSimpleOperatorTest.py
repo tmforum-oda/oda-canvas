6de9a1ddcc8d22ed2b11a853612f8ab8d0c23b11
Ferenc Hechler <ferenc.hechler@telekom.de>
2024-09-18 13:54:12 +0200

    format with black.

--- source/operators/dependentApiSimpleOperator/docker/manual_test/dependentApiSimpleOperatorTest.py
+++ source/operators/dependentApiSimpleOperator/docker/manual_test/dependentApiSimpleOperatorTest.py
@@ -4,9 +4,7 @@ import logging
 import kopf
 import asyncio
 
-os.environ["CANVAS_INFO_ENDPOINT"] = (
-    "http://localhost:8638"
-)
+os.environ["CANVAS_INFO_ENDPOINT"] = "http://localhost:8638"
 
 sys.path.append("../src")
 from dependentApiSimpleOperator import (
