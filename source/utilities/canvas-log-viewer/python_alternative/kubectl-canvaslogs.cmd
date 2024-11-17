f3006d44e5c05d209eb82b1fcddc9e09c75ec212
Ferenc Hechler <github@fh.anderemails.de>
2024-11-17 23:32:17 +0100

    support componentfilter in kubectl canvaslog

--- source/utilities/canvas-log-viewer/python_alternative/kubectl-canvaslogs.cmd
+++ source/utilities/canvas-log-viewer/python_alternative/kubectl-canvaslogs.cmd
@@ -1,16 +1,16 @@
 @echo off
 
-if "%1" == "smanop" (
-	kubectl logs -n canvas deployment/canvas-smanop | python showlogtree.py
+if "%1" == "comp" (
+	kubectl logs -n canvas deployment/component-operator | python showlogtree.py "%2"
 	exit 0
 )
-if "%1" == "compop" (
-	kubectl logs -n canvas deployment/component-operator | python showlogtree.py
+if "%1" == "sman" (
+	kubectl logs -n canvas deployment/canvas-smanop | python showlogtree.py "%2"
 	exit 0
 )
-if [%1] == [] (
-	kubectl logs -n canvas deployment/component-operator | python showlogtree.py
+if "%1" == "depapi" (
+	kubectl logs -n canvas deployment/canvas-depapi-op | python showlogtree.py "%2"
 	exit 0
 )
-echo "usage: kubectl canvaslogs [compop|smanop]"
+echo "usage: kubectl canvaslogs (comp|sman|depapi) [componentfilter]"
 echo "       needs python with 'pip install rich'"
--- source/utilities/canvas-log-viewer/python_alternative/showlogtree.py
+++ source/utilities/canvas-log-viewer/python_alternative/showlogtree.py
@@ -5,9 +5,10 @@ from rich import print
 
 from rich.console import Console
 from rich.text import Text
-    
+
 
 import re
+import fnmatch
 import json
 
 # [2024-11-17 13:05:34,612] SManOP               [INFO    ] [demo-a-productcatalogmanagement|SMan/demo-a-productcatalogmanagement|secretsmanagementDelete|secretsmanagementDelete] Delete  called with namespace: components
@@ -24,8 +25,13 @@ FUNCTION_RX = "(?P<function>[^\\]]*)\\]"
 SUBJECT_RX = "(?P<subject>[^:]+):"
 MESSAGE_RX = "(?P<message>.*)"
 
-LINE_RX = re.compile(f"{TIME_RX}{sp}{LOGGER_RX}{sp}{LEVEL_RX}{sp}({COMPONENT_RX}{RESOURCE_RX}{HANDLER_RX}{FUNCTION_RX}{sp}{SUBJECT_RX}{sp}|{RESOURCE_ONLY_RX}{sp}|){MESSAGE_RX}")
-LINE_RO_RX = re.compile(f"{TIME_RX}{sp}{LOGGER_RX}{sp}{LEVEL_RX}{sp}{RESOURCE_ONLY_RX}{sp}{MESSAGE_RX}")
+LINE_RX = re.compile(
+    f"{TIME_RX}{sp}{LOGGER_RX}{sp}{LEVEL_RX}{sp}({COMPONENT_RX}{RESOURCE_RX}{HANDLER_RX}{FUNCTION_RX}{sp}{SUBJECT_RX}{sp}|{RESOURCE_ONLY_RX}{sp}|){MESSAGE_RX}"
+)
+LINE_RO_RX = re.compile(
+    f"{TIME_RX}{sp}{LOGGER_RX}{sp}{LEVEL_RX}{sp}{RESOURCE_ONLY_RX}{sp}{MESSAGE_RX}"
+)
+
 
 def nvl(obj, none_value):
     if obj is None:
@@ -33,22 +39,23 @@ def nvl(obj, none_value):
     else:
         return str(obj)
 
+
 def parse_log(lines):
     result = []
-    last_entry = {"message":""}
+    last_entry = {"message": ""}
     for line in lines:
         m = LINE_RX.match(line)
         if m:
             entry = {
-                "time": m.group('time'),
-                "logger":    m.group('logger'),
-                "level":     m.group('level'),
-                "component": nvl(m.group('component'), ""),
-                "resource":  nvl(m.group('resource'), nvl(m.group('resourceonly'), "")),
-                "handler":   nvl(m.group('handler'), ""),
-                "function":  nvl(m.group('function'), ""),
-                "subject":   nvl(m.group('subject'), ""),
-                "message":   m.group('message')
+                "time": m.group("time"),
+                "logger": m.group("logger"),
+                "level": m.group("level"),
+                "component": nvl(m.group("component"), ""),
+                "resource": nvl(m.group("resource"), nvl(m.group("resourceonly"), "")),
+                "handler": nvl(m.group("handler"), ""),
+                "function": nvl(m.group("function"), ""),
+                "subject": nvl(m.group("subject"), ""),
+                "message": m.group("message"),
             }
             result.append(entry)
             last_entry = entry
@@ -57,13 +64,25 @@ def parse_log(lines):
     return result
 
 
-def create_log_tree(filename):
-    files = [] if filename=='-' else [filename]
-    lines = list(fileinput.input(files = files))           
-    #lines = lines[0:20]
+def checkFilter(compname, compfilter):
+    if not compfilter:
+        return True
+    if compname == compfilter:
+        return True
+    if not compname:
+        return False
+    return fnmatch.fnmatch(compname, compfilter)
+
+
+def create_log_tree(filename, compfilter):
+    files = [] if filename == "-" else [filename]
+    lines = list(fileinput.input(files=files))
+    # lines = lines[0:20]
     entries = parse_log(lines)
-    logTree = {} 
+    logTree = {}
     for entry in entries:
+        if not checkFilter(entry["component"], compfilter):
+            continue
         next = logTree
         if entry["component"] not in next:
             next[entry["component"]] = {}
@@ -80,51 +99,55 @@ def create_log_tree(filename):
         if entry["function"] not in next:
             next[entry["function"]] = []
         next = next[entry["function"]]
-        message = f'{entry["subject"]}: {entry["message"]}' if entry["subject"] else entry["message"]
+        message = (
+            f'{entry["subject"]}: {entry["message"]}'
+            if entry["subject"]
+            else entry["message"]
+        )
 
         text = Text()
-        text.append(entry["time"]+' ', style="#808080")
+        text.append(entry["time"] + " ", style="#808080")
         if entry["function"]:
-            text.append(entry["function"] + ': ')
-        if entry["level"] == 'INFO':
+            text.append(entry["function"] + ": ")
+        if entry["level"] == "INFO":
             text.append(message, style="green")
-        elif entry["level"] == 'ERROR':
+        elif entry["level"] == "ERROR":
             text.append(message, style="red")
-        elif entry["level"] == 'WARNING':
+        elif entry["level"] == "WARNING":
             text.append(message, style="yellow")
-        elif entry["level"] == 'DEBUG':
+        elif entry["level"] == "DEBUG":
             text.append(message, style="lightblue")
         else:
             text.append(message)
         next.append(text)
     return logTree
-        
+
+
 def recursive_build_tree(input_tree, output_tree):
     if isinstance(input_tree, list):
         for line in input_tree:
             output_tree.add(line)
         return
-    for k,v in input_tree.items():
+    for k, v in input_tree.items():
         if k:
             child = output_tree.add(k)
         else:
             child = output_tree
         recursive_build_tree(v, child)
 
-def show_log_tree(filename):
-    logTree = create_log_tree(filename)
+
+def show_log_tree(filename, compfilter):
+    logTree = create_log_tree(filename, compfilter)
     tree = Tree(f"[green]{filename}")
     recursive_build_tree(logTree, tree)
     print(tree)
-    
 
 
 if __name__ == "__main__":
-    if len(sys.argv)>1:
-        filename = sys.argv[1]
+    if len(sys.argv) > 1:
+        compfilter = sys.argv[1]
     else:
-        filename = "-" 
-    #filename = "canvas-smanop.log"
-    #filename = "canvas-compop.log"
-    show_log_tree(filename)
-    
+        compfilter = None
+    # filename = "canvas-smanop.log"
+    # filename = "canvas-compop.log"
+    show_log_tree("-", compfilter)
