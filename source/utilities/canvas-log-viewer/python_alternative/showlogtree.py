b1d8dde9c43f2b7ff2fb33f6aacf903cbae49b7d
Ferenc Hechler <ferenc.hechler@telekom.de>
2024-11-20 08:59:28 +0100

    [skip tests] update python Canvas Log Viewer to support -f and -l <lasthours>

--- source/utilities/canvas-log-viewer/python_alternative/showlogtree.py
+++ source/utilities/canvas-log-viewer/python_alternative/showlogtree.py
@@ -1,7 +1,9 @@
 import sys
 import fileinput
+import datetime
+import argparse
 from rich.tree import Tree
-from rich import print
+from rich import print as rich_print
 
 from rich.console import Console
 from rich.text import Text
@@ -10,6 +12,17 @@ from rich.text import Text
 import re
 import fnmatch
 import json
+
+from timedinput import timedinput
+from _queue import Empty
+
+import asyncio
+import queue
+import threading
+
+
+config = {}
+
 
 # [2024-11-17 13:05:34,612] SManOP               [INFO    ] [demo-a-productcatalogmanagement|SMan/demo-a-productcatalogmanagement|secretsmanagementDelete|secretsmanagementDelete] Delete  called with namespace: components
 # [<TIME>] <LOGGER> [<LEVEL>] [<COMPONENT>|<RESOURCE>|<HANDLER>|<FUNCTION>] <SUBJECT>: <MESSAGE>
@@ -61,7 +74,7 @@ def parse_log(lines):
     return result
 
 
-def checkFilter(compname, compfilter):
+def checkCompFilter(compname, compfilter):
     if not compfilter:
         return True
     if compname == compfilter:
@@ -71,14 +84,51 @@ def checkFilter(compname, compfilter):
     return fnmatch.fnmatch(compname, compfilter)
 
 
-def create_log_tree(filename, compfilter):
-    files = [] if filename == "-" else [filename]
-    lines = list(fileinput.input(files=files))
-    # lines = lines[0:20]
+def checkTimeFilter(time, mintimefilter):
+    if mintimefilter is None:
+        return True
+    return time >= mintimefilter
+
+
+def calc_mintime_filter():
+    lasthours = config["lasthours"]
+    if not lasthours:
+        return None
+    tshifth = config["tshifth"]
+    if tshifth:
+        lasthours = lasthours - tshifth
+    datetimeformat = config["datetimeformat"]
+    now = datetime.datetime.now()
+    mintime = now - datetime.timedelta(hours=lasthours)
+    return mintime.strftime(datetimeformat)
+
+
+def create_log_tree():
+    filename = config["filename"]
+    compfilter = config["compfilter"]
+    mintimefilter = calc_mintime_filter()
+    if filename == "-":  # stdin
+        if config["follow"]:
+            sysinq = config["sysinq"]
+            lines = []
+            try:
+                while True:
+                    line = sysinq.get(timeout=1.0)
+                    lines.append(line)
+            except Empty:
+                pass
+        else:
+            lines = list(fileinput.input(files=[]))
+    else:
+        if config["follow"]:
+            raise ValueError("--follow not yet supported with file inputs")
+        lines = list(fileinput.input(files=[filename]))
     entries = parse_log(lines)
     logTree = {}
     for entry in entries:
-        if not checkFilter(entry["component"], compfilter):
+        if not checkCompFilter(entry["component"], compfilter):
+            continue
+        if not checkTimeFilter(entry["time"], mintimefilter):
             continue
         next = logTree
         if entry["component"] not in next:
@@ -133,14 +183,85 @@ def recursive_build_tree(input_tree, output_tree):
         recursive_build_tree(v, child)
 
 
-def show_log_tree(filename, compfilter):
-    logTree = create_log_tree(filename, compfilter)
-    tree = Tree(f"[green]Canvas Log Viewer ({filename})")
-    recursive_build_tree(logTree, tree)
-    print(tree)
+def show_log_tree():
+    first_time = True
+    while True:
+        logTree = create_log_tree()
+        if first_time or logTree:
+            tree = Tree(f'[green]Canvas Log Viewer ({config["filename"]})')
+            recursive_build_tree(logTree, tree)
+            rich_print(tree)
+        if not config["follow"]:
+            break
+        first_time = False
+
+
+def sysinreader():
+    sysinq = config["sysinq"]
+    while True:
+        line = sys.stdin.readline()
+        sysinq.put(line.rstrip())
 
 
 if __name__ == "__main__":
-    compfilter = sys.argv[1] if len(sys.argv) > 1 else None
-    filename = "-"  # stdin
-    show_log_tree(filename, compfilter)
+    parser = argparse.ArgumentParser(
+        prog="Canvas Log Viewer", description="display tree view of logfiles"
+    )
+    parser.add_argument(
+        "-c",
+        "--component-filter",
+        type=str,
+        help='filter components by name with wildcards (*/?), e.g. "comp-a-*"',
+        required=False,
+    )
+    parser.add_argument(
+        "-d",
+        "--datetime-format",
+        type=str,
+        help='format of timestaps in log, default "%%Y-%%m-%%d %%H:%%M:%%S,000"',
+        default="%Y-%m-%d %H:%M:%S,000",
+    )
+    parser.add_argument(
+        "-f",
+        "--follow",
+        action=argparse.BooleanOptionalAction,
+        help="keep the Log Viewer open and update the display on incoming logs",
+    )
+    parser.add_argument(
+        "-l",
+        "--last-hours",
+        type=float,
+        required=False,
+        help="only look at the last n hours",
+    )
+    parser.add_argument(
+        "-i",
+        "--input-file",
+        type=str,
+        help="use file as input instead of stdin",
+        default="-",
+    )
+    parser.add_argument(
+        "-t",
+        "--time-shift-hours",
+        type=float,
+        help="log files have a time shift by t hours",
+        default=0.0,
+    )
+
+    # testargs = ["-i", "depapi.log", "-l", "8", "-t", "-1", "-d", "%Y-%m-%d %H:%M:%S,000", "-c", "testdapi-*"]
+    # args = parser.parse_args(testargs)
+
+    args = parser.parse_args()
+
+    config["compfilter"] = args.component_filter
+    config["datetimeformat"] = args.datetime_format
+    config["follow"] = bool(args.follow)
+    config["lasthours"] = args.last_hours
+    config["filename"] = args.input_file
+    config["tshifth"] = args.time_shift_hours
+
+    if config["follow"]:
+        config["sysinq"] = queue.Queue()
+        threading.Thread(target=sysinreader, daemon=True).start()
+    show_log_tree()
