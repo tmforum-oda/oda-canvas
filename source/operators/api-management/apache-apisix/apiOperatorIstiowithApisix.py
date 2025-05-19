d3ab047e1b04c21e1f44781ed147142f6fc938b8
Lester <lesterthomas@hotmail.com>
2025-05-19 15:33:16 +0100

    Added mcp and openmetrics to CRD and extended API Operators to allow prometheus or openmetrics.

--- source/operators/api-management/apache-apisix/apiOperatorIstiowithApisix.py
+++ source/operators/api-management/apache-apisix/apiOperatorIstiowithApisix.py
@@ -184,7 +184,7 @@ def apiStatus(meta, spec, status, namespace, labels, name, **kwargs):
                 )
                 # if the apitype of the api is 'prometheus' then we need to also create a ServiceMonitor resource
                 if "apitype" in spec.keys():
-                    if spec["apitype"] == "prometheus":
+                    if spec["apiType"] == "prometheus" or spec["apiType"] == "openmetrics":
                         # create a ServiceMonitor resource
                         logWrapper(
                             logging.INFO,
@@ -214,7 +214,7 @@ def apiStatus(meta, spec, status, namespace, labels, name, **kwargs):
     )
     # if the apitype of the api is 'prometheus' then we need to also create a ServiceMonitor resource
     if "apitype" in spec.keys():
-        if spec["apitype"] == "prometheus":
+        if spec["apiType"] == "prometheus" or spec["apiType"] == "openmetrics":
             # create a ServiceMonitor resource
             logWrapper(
                 logging.INFO,
