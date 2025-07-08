f832b164201400aa3dab6063bb7d37e274154b9d
ferenc-hechler <github@fh.anderemails.de>
2025-07-08 23:56:30 +0200

    added model for ExposedAPI

--- source/services/ComponentRegistry/src/app/model.py
+++ source/services/ComponentRegistry/src/app/model.py
@@ -1,7 +1,13 @@
 # from https://sqlmodel.tiangolo.com/tutorial/fastapi/simple-component-api/#install-fastapi
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
 from sqlalchemy.sql.schema import Column
 from sqlalchemy.sql.sqltypes import JSON
+
+# see https://sqlmodel.tiangolo.com/tutorial/fastapi/relationships/#why-arent-we-getting-more-data
+
+    # ========== #
+    #  COMPONENT #
+    # ========== #
 
 
 class ComponentBase(SQLModel):
@@ -9,15 +15,57 @@ class ComponentBase(SQLModel):
     owner: str
     labels: dict[str, str] = Field(default={"key": "value"}, sa_column=Column(JSON))
 
+
 class Component(ComponentBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
+    
+    exposed_apis: list["ExposedAPI"] = Relationship(back_populates="component")
 
-class ComponentCreate(ComponentBase):
-    pass
 
 class ComponentPublic(ComponentBase):
     id: int
     
+
+class ComponentCreate(ComponentBase):
+    pass
+
+
 class ComponentUpdate(SQLModel):
     labels: dict[str, str|None] | None = None
 
+
+
+    # ============ #
+    #  EXPOSED API #
+    # ============ #
+
+
+class ExposedAPIBase(SQLModel):
+    specification_url: str = Field(index=True)
+    specification_version: str
+    name: str
+    description: str | None
+    apiType: str
+    ready: bool
+    url: str
+    
+    component_id: int | None = Field(default=None, foreign_key="component.id")
+
+
+class ExposedAPI(ExposedAPIBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+    component: Component | None = Relationship(back_populates="exposed_apis")
+
+
+class ExposedAPIPublic(ExposedAPIBase):
+    id: int
+
+
+class ExposedAPICreate(ExposedAPIBase):
+    pass
+
+
+class ExposedAPIUpdate(SQLModel):
+    ready: bool | None = None
+    url: str | None = None
