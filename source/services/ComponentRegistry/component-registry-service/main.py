c795f30731b3a6399a3d62765e8eb99cdf06dd66
Ferenc Hechler <ferenc.hechler@telekom.de>
2025-09-30 00:11:31 +0200

    fix ingress

--- source/services/ComponentRegistry/component-registry-service/main.py
+++ source/services/ComponentRegistry/component-registry-service/main.py
@@ -217,4 +217,4 @@ async def root():
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
--- source/services/ComponentRegistry/component-registry-service/run.py
+++ source/services/ComponentRegistry/component-registry-service/run.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     uvicorn.run(
         app,
         host="0.0.0.0",
-        port=8000,
+        port=8080,
         log_level="info",
         reload=False
     )
