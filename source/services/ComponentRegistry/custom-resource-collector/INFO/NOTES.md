aa7621eb14bc8a873fdb57638f25a0e67de3ab6d
Ferenc Hechler <ferenc.hechler@telekom.de>
2025-09-30 19:01:25 +0200

    create cronjob for syncing component registry

---
+++ source/services/ComponentRegistry/custom-resource-collector/INFO/NOTES.md
@@ -0,0 +1,7 @@
+# Deploy ComponentRegistry Helm Charts
+
+```
+helm upgrade --install collector -n compreg --create-namespace helm/custom-resource-collector
+```
+
+
--- source/services/ComponentRegistry/custom-resource-collector/build.sh
+++ source/services/ComponentRegistry/custom-resource-collector/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-VERSION=0.1.0
+VERSION=0.1.2
 docker build --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -t ocfork/custom-resource-collector:$VERSION .
 docker push ocfork/custom-resource-collector:$VERSION
