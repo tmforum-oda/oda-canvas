ca426efe968673de020ecfba41db0e3b147f8b5b
Ferenc Hechler <ferenc.hechler@telekom.de>
2026-06-12 12:34:27 +0200

    fix using an always existent deployment

--- feature-definition-and-test-kit/utilities/component-utils/component-utils.js
+++ feature-definition-and-test-kit/utilities/component-utils/component-utils.js
@@ -230,7 +230,7 @@ const componentUtils = {
   getDebugInfoDepApis: function () {
     try {
 	  const debugInfo1 = execSync(`kubectl get dependentapis,exposedapis,components -A`, { encoding: 'utf-8' });
-	  const debugInfo2 = execSync(`kubectl exec -n components deployment/r-cat-prodcatapi -- curl -s http://info.canvas.svc.cluster.local/service -H "accept: application/json"`, { encoding: 'utf-8' });
+	  const debugInfo2 = execSync(`kubectl exec -n canvas deployment/canvas-info-service -- curl -s http://info.canvas.svc.cluster.local/service -H "accept: application/json"`, { encoding: 'utf-8' });
       return `${debugInfo1}\n\n--- info service ---\n\n${debugInfo2}`;
     } catch (error) {
       console.error(`Error getting debug infos for dependent apis: ${error.message}`);
