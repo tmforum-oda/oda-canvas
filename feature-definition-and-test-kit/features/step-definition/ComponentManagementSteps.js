6c197a0114b121b7fbed6db0ee723244cc3388bd
Ferenc Hechler <ferenc.hechler@telekom.de>
2026-06-13 01:07:15 +0200

    remove component yaml logging

--- feature-definition-and-test-kit/features/step-definition/ComponentManagementSteps.js
+++ feature-definition-and-test-kit/features/step-definition/ComponentManagementSteps.js
@@ -378,7 +378,7 @@ When('the {string} component has a deployment status of {string} or {string}', {
       }
     }
   }
-  console.log(`Component '${componentName}' in namespace '${namespace}: ` + JSON.stringify(componentResource));
+  // console.log(`Component '${componentName}' in namespace '${namespace}: ` + JSON.stringify(componentResource));
 });
 
 /**
